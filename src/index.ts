8f32ba3ea37a670d7e5a0ad9c98055096772af5b
Darsh <darshanpatil142003@gmail.com>
2025-08-17 18:38:00 +0530

    medium blog hono add

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,89 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-const app = new Hono()
+import { decode, sign, verify } from 'hono/jwt'
+
+
+const app = new Hono<{ Bindings: { DATABASE_URL: string } }>()
+
+
+app.use('/api/v1/blog/*', async (c, next) => {
+  const jwt = c.req.header('Authorization');
+  if (!jwt) {
+    c.status(401);
+    return c.json({ error: "unauthorized" });
+  }
+  const token = jwt.split(' ')[1];
+  const payload = await verify(token, "secret");
+  if (!payload) {
+    c.status(401);
+    return c.json({ error: "unauthorized" });
+  }
+  await next()
+})
 
 app.get('/', (c) => {
-  // Get query parameter like /?param=test
-  const param = c.req.query('param')
-  console.log('Query param:', param)
-
-  // Get header like Authorization: Bearer token
-  const authHeader = c.req.header('Authorization')
-  console.log('Authorization header:', authHeader)
-
-  // No need to parse JSON body on GET (it should be in POST/PUT)
-  return c.text('Hello Hono!')
+  return c.text("hellooooo");
 })
+
+
+app.post('/api/v1/signup', async (c) => {
+
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  const body = await c.req.json();
+  const user = await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password
+    }
+  })
+
+  const token = await sign({ id: user.id }, "secret");
+  return c.json({
+    jwt: token
+  })
+})
+
+app.post('/api/v1/signin', async (c) => {
+
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  const body = await c.req.json();
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: body.email,
+      password: body.password
+    }
+  });
+
+  if (!user) {
+    c.status(403);
+    return c.json({ error: "user not found" })
+  }
+
+  const token = sign({ id: user.id }, "secret");
+  return c.json({ jwt: token });
+
+
+})
+
+// app.post('/api/v/blog', (c) => {
+//   return c.text("hello");
+// })
+
+// app.put('/api/v1/blog', (c) => {
+
+// })
+
+// app.get('/api/v1/blog/:id', (c) => {
+
+// })
 
 export default app
