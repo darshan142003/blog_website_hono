79468f1a8ac7e5eb16910953c426a1566195ffd6
Darsh <darshanpatil142003@gmail.com>
2025-08-17 23:00:28 +0530

    deployed medium on cloudflare workers

---
+++ src/routes/user.ts
@@ -0,0 +1,67 @@
+import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+import { decode, sign, verify } from 'hono/jwt'
+
+
+export const userRouter = new Hono<{ Bindings: { DATABASE_URL: string } }>()
+
+
+userRouter.post('/signup', async (c) => {
+
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
+
+    const body = await c.req.json();
+    try {
+        const user = await prisma.user.create({
+            data: {
+                email: body.email,
+                password: body.password
+            }
+        })
+
+        const token = await sign({ id: user.id }, "secret");
+        return c.json({
+            jwt: token
+        })
+    } catch (e) {
+        console.error("Signup error:", e); // Helps you debug
+        c.status(400); // Use 400 for bad request
+        return c.json({ error: "Request failed", details: String(e) });
+    }
+})
+
+userRouter.post('/signin', async (c) => {
+
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
+
+    const body = await c.req.json();
+
+    try {
+
+        const user = await prisma.user.findUnique({
+            where: {
+                email: body.email,
+                password: body.password
+            }
+        });
+
+        if (!user) {
+            c.status(403);
+            return c.json({ error: "Invalid Cred" })
+        }
+
+        const token = await sign({ id: user.id }, "secret");
+        return c.json({ jwt: token });
+    } catch (e) {
+        c.status(403);
+        return c.json({ error: "Invalid" })
+    }
+
+
+})
